84bced0a9dd30dace325d96a6675a51fe166924e
chaeyoung <ocy5007@naver.com>
2024-05-26 23:34:53 +0900

    design: 나의 증상, 상황 퍼블리싱

--- src/components/counseling/mysituation.tsx
+++ src/components/counseling/mysituation.tsx
@@ -1,9 +1,22 @@
-import './index.css'
+import '../../index.css'
 
 const MySituation = () => {
   return (
-    <div className="flex flex-col justify-center items-center w-13.5625rem h-26.375rem border-0.0625rem">
-      <p>나의 상황</p>
+    <div className="flex flex-col justify-center items-center w-[217px] h-[422px] border-[1px] space-y-[47px]">
+      <p className="bodylbold">나의 상황</p>
+      <div className="grid grid-cols-2 gap-[30px]">
+        <div>결혼</div>
+        <div>육아</div>
+        <div>대인관계</div>
+        <div>직장</div>
+        <div>정신건강</div>
+        <div>사회생활</div>
+        <div>성추행</div>
+        <div>신체건강</div>
+        <div>마약/도박</div>
+        <div>자아/성격</div>
+        <div>금전/사업</div>
+      </div>
     </div>
   )
 }
