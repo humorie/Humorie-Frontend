b05a5407630b883fe26362fd71c589957ab08a3d
chaeyoung <ocy5007@naver.com>
2024-05-26 23:38:57 +0900

    design: Counseling 페이지 전체 퍼블리싱

--- src/pages/Counseling.tsx
+++ src/pages/Counseling.tsx
@@ -1,8 +1,28 @@
+import Counselorlist from '../components/counseling/counselorlist'
+import Dropdown from '../components/counseling/dropdown'
+import FindCounselor from '../components/counseling/findcounselor'
 import MySituation from '../components/counseling/mysituation'
-import './index.css'
+import MySymptoms from '../components/counseling/mysymptoms'
+import Recommendation from '../components/counseling/recommendation'
+import '../index.css'
 
 const Counseling: React.FC = () => {
-  return <MySituation />
+  return (
+    <div className="flex flex-col justify-center items-center w-screen space-y-[50px] scroll-auto">
+      <FindCounselor />
+      <div className="flex flex-row w-[1050px] justify-between">
+        <div className="flex flex-col space-y-[24px]">
+          <MySituation />
+          <MySymptoms />
+        </div>
+        <div className="flex flex-col space-y-[21px]">
+          <Dropdown />
+          <Recommendation />
+          <Counselorlist />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Counseling
