c1ea98f0e52663e621314c4539b9bd369c11fe23
gmltn328 <gmltn328@gmail.com>
2024-07-06 01:45:16 +0900

    design: 상담내역, 상담앤역 상세페이지 퍼블리싱

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -1,8 +1,8 @@
-import Contents from '../components/history/Contents'
-import Header from '../components/header/Header'
 import Footer from '../components/Footer'
+import Header from '../components/header/Header'
+import Contents from '../components/post/Contents'
 
-const History: React.FC = () => {
+const Post: React.FC = () => {
   return (
     <div className="flex flex-col items-center justify-center bg-white">
       <Header />
@@ -12,4 +12,4 @@ const History: React.FC = () => {
   )
 }
 
-export default History
+export default Post
