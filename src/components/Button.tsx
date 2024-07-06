f1e34dc04d5b0c1747331244b4704465fddc8a12
gmltn328 <gmltn328@gmail.com>
2024-07-06 14:43:20 +0900

    merge from main

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,25 +2,26 @@ interface ButtonProps {
   label: string // 버튼 이름 prop
   onClick?: () => void // onClick prop
   size: 'XLarge' | 'Large' | 'Medium' | 'Small' | 'XSmall' // 버튼 크기를 선택하기 위한 prop
+  color?: 'pink' | 'gray' // 색상 선택을 위한 prop
 }
 
-const Button: React.FC<ButtonProps> = ({ label, onClick, size }) => {
+const Button: React.FC<ButtonProps> = ({ label, onClick, size, color = 'pink' }) => {
+  const colorClasses = {
+    pink: 'bg-primary-600 text-white hover:bg-primary-700 hover:text-gray-50 active:bg-primary-800 active:text-white',
+    gray: 'bg-gray-200 text-gray-500 hover:bg-gray-200 hover:text-gray-600 active:bg-gray-300 active:text-gray-600',
+  }
+
   const itemVariants = {
-    XLarge:
-      'bodyxssemibold flex h-[48px] w-[312px] items-center justify-center gap-2 rounded bg-primary-600 text-white hover:bg-primary-700 hover:text-gray-50 active:bg-primary-800 active:text-white',
-    Large:
-      'bodyxssemibold flex h-[48px] w-[160px] items-center justify-center gap-2 rounded bg-primary-600 text-white hover:bg-primary-700 hover:text-gray-50 active:bg-primary-800 active:text-white',
-    Medium:
-      'bodyxssemibold flex h-[40px] w-[125px] items-center justify-center gap-2 rounded bg-primary-600 text-white hover:bg-primary-700 hover:text-gray-50 active:bg-primary-800 active:text-white',
-    Small:
-      'bodyxssemibold flex h-[32px] w-[102px] items-center justify-center gap-2 rounded bg-primary-600 text-white hover:bg-primary-700 hover:text-gray-50 active:bg-primary-800 active:text-white',
-    XSmall:
-      'bodyxssemibold flex h-[32px] w-[60px] items-center justify-center gap-2 rounded bg-primary-600 text-white hover:bg-primary-700 hover:text-gray-50 active:bg-primary-800 active:text-white',
+    XLarge: `bodymdsemibold flex h-[48px] w-[312px] items-center justify-center gap-2 rounded ${colorClasses[color]}`,
+    Large: `bodymdsemibold flex h-[48px] w-[160px] items-center justify-center gap-2 rounded ${colorClasses[color]}`,
+    Medium: `bodysmsemibold flex h-[40px] w-[125px] items-center justify-center gap-2 rounded ${colorClasses[color]}`,
+    Small: `bodyxssemibold flex h-[32px] w-[102px] items-center justify-center gap-2 rounded ${colorClasses[color]}`,
+    XSmall: `bodyxssemibold flex h-[32px] w-[60px] items-center justify-center gap-2 rounded ${colorClasses[color]}`,
   }
 
   return (
     <div
-      className={itemVariants[size]} // 버튼 크기에 따른 스타일을 동적으로 적용
+      className={`${itemVariants[size]} cursor-pointer`} // 버튼 크기 및 색상에 따른 스타일을 동적으로 적용
       onClick={onClick} // onClick 핸들러
     >
       {label}
