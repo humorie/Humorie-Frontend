4b1ed04224bb82b2d4fa90d9d696a647caf6274b
chaeyoung <ocy5007@naver.com>
2024-07-13 12:49:22 +0900

    design: zustand store 추가

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -12,17 +12,44 @@ export const useModalStore = create<ModalState>((set) => ({
   closeModal: () => set({ modalOpen: false }),
 }))
 
-interface Tag {
+export interface Tag {
   title: string
   content: string
 }
 
 interface TagsState {
   tags: Tag[]
+  selectedSymptoms: string[]
   addTag: (tag: Tag) => void
+  removeTag: (content: string) => void
+  addSelectedSymptom: (symptom: string) => void
+  removeSelectedSymptom: (symptom: string) => void
 }
 
 export const useTagsStore = create<TagsState>((set) => ({
   tags: [],
+  selectedSymptoms: [],
   addTag: (tag: Tag) => set((state) => ({ tags: [...state.tags, tag] })),
+  removeTag: (content: string) =>
+    set((state) => ({
+      tags: state.tags.filter((tag) => tag.content !== content),
+    })),
+  addSelectedSymptom: (symptom) =>
+    set((state) => ({ selectedSymptoms: [...state.selectedSymptoms, symptom] })),
+  removeSelectedSymptom: (symptom) =>
+    set((state) => ({ selectedSymptoms: state.selectedSymptoms.filter((s) => s !== symptom) })),
 }))
+
+interface DropdownState {
+  selectedDropdowns: { [key: string]: string }
+  setSelectedDropdown: (dropdownKey: string, value: string) => void
+}
+
+const useDropdownStore = create<DropdownState>((set) => ({
+  selectedDropdowns: {},
+  setSelectedDropdown: (dropdownKey, value) =>
+    set((state) => ({
+      selectedDropdowns: { ...state.selectedDropdowns, [dropdownKey]: value },
+    })),
+}))
+export default useDropdownStore
