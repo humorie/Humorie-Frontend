828f00a63560f0c5c3ee187546550cf3deb93669
chaeyoung <ocy5007@naver.com>
2024-07-14 00:36:00 +0900

    design: footer에서 해당페이지 위치시, 텍스트 색깔 변경

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,7 @@
-import { useNavigate } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 
 const Footer: React.FC = () => {
+  const location = useLocation()
   const navigate = useNavigate()
   const onClickCustomerServicehandler = () => {
     navigate('/customerservice')
@@ -18,12 +19,14 @@ const Footer: React.FC = () => {
               {/* 고객센터, 이용약관 */}
               <div className="bodysmbold flex items-center justify-start text-center text-gray-800">
                 <div
-                  className="flex h-[60px] w-[100px] cursor-pointer items-center justify-center text-center hover:text-primary-700 active:text-primary-800"
+                  className={`flex h-[60px] w-[100px] cursor-pointer items-center justify-center text-center hover:text-primary-700 active:text-primary-800 
+                    ${location.pathname === '/customerservice' ? 'text-primary-700' : 'text-gray-800'}`}
                   onClick={onClickCustomerServicehandler}>
                   고객센터
                 </div>
                 <div
-                  className="flex h-[60px] w-[100px] cursor-pointer items-center justify-center text-center hover:text-primary-700 active:text-primary-800"
+                  className={`flex h-[60px] w-[100px] cursor-pointer items-center justify-center text-center hover:text-primary-700 active:text-primary-800
+                    ${location.pathname === '/policy' ? 'text-primary-800' : 'text-gray-800'}`}
                   onClick={onClickPolicyhandler}>
                   이용약관
                 </div>
