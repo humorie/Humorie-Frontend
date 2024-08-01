5ce9a41b02f5d5e20ef55a971d5b8845f1b0c456
gmltn328 <gmltn328@gmail.com>
2024-08-01 16:52:43 +0900

    Comment: CORS에러에 관한 프록시 설정 주석 추가

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
+// CORS 에러 해결을 위해 백엔드 서버를 프록시 설정으로 해결했지만 이게 정답인지 확인이 필요하다
+// 임시방편으로 우회를 한게 아닐까 생각됨
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
