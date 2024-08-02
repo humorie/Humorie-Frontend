c0377bd83ad84da12c4ed14318b8570df93d6014
gmltn328 <gmltn328@gmail.com>
2024-08-02 18:06:48 +0900

    Design: 최근 상담내역 페이지 디자인 수정 및 더미데이터 제거

--- src/components/history/Contents.tsx
+++ src/components/history/Contents.tsx
@@ -1,13 +1,13 @@
-import RecentPost from './RecentPost'
+// import RecentPost from './RecentPost'
 import Posts from './Posts'
 
 const Contents: React.FC = () => {
   return (
     <div className="flex w-[1060px] flex-col items-center justify-center bg-white text-black">
-      <div className="mt-[60px] flex h-[93px] w-full flex-col items-center justify-center">
+      <div className="mt-[85px] flex h-[93px] w-full flex-col items-center justify-center">
         <p className="mdbold">상담 내역을 확인해 볼까요?</p>
       </div>
-      <RecentPost />
+      {/* <RecentPost /> */}
       <Posts />
     </div>
   )
--- src/components/history/EmptyPost.tsx
+++ src/components/history/EmptyPost.tsx
@@ -2,17 +2,15 @@ const EmptyPost: React.FC = () => {
   return (
     <div className="flex w-[1060px] flex-col items-center justify-center bg-white">
       <div className="mt-[73px] flex flex-col">
-        <p className="text-center text-sm font-semibold text-black">최근 상담내역이 없습니다.</p>
+        <p className="bodylsemibold text-black">최근 상담내역이 없습니다.</p>
       </div>
 
-      <div className="mb-[381px] mt-[168px] flex h-[42px] w-[273px] flex-col items-center justify-center gap-2 rounded-md bg-pink-400">
-        <div className="flex items-center justify-center gap-2">
-          <p className="w-[130px] text-center text-sm font-semibold leading-normal tracking-tight text-white">
-            상담 예약하러 가기
-          </p>
-          <div className="relative h-5 w-5" />
-        </div>
-      </div>
+      <button
+        className="bodysmmedium mb-[381px] mt-[168px] flex h-[42px] w-[273px] flex-row items-center justify-center gap-2 rounded-md bg-pink-400 text-white"
+        onClick={() => alert('상담사별 예약페이지로 라우팅')}>
+        상담 예약하러 가기
+        <img src="/src/assets/images/icon/icon_arrow_right_white.svg" alt="오른쪽 흰색 화살표" />
+      </button>
     </div>
   )
 }
