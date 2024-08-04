b5bdc46a636a00010929c5133403ae2962a5c6f3
chaeyoung <ocy5007@naver.com>
2024-08-04 16:19:35 +0900

    feat: 상담사 페이지 API 연동

--- src/components/counselor/reviewcard.tsx
+++ src/components/counselor/reviewcard.tsx
@@ -1,27 +1,34 @@
 import '../../index.css'
 
-const ReviewCard = () => {
+interface Review {
+  content: string
+  rating: number
+  createdAt: string
+}
+
+interface ReviewCardProps {
+  review: Review
+}
+
+const ReviewCard: React.FC<ReviewCardProps> = ({ review }) => {
   return (
-    <div className="flex flex-col justify-between w-[303px] h-[207px] bg-gray-100 py-[20px] px-[18px] rounded-[15px]">
+    <div className="flex h-[207px] w-[303px] flex-col justify-between rounded-[15px] bg-gray-100 px-[18px] py-[20px]">
       <div className="flex flex-row items-center">
         <img
           src="/src/assets/images/counseling/star_rate.svg"
           alt="rate"
-          className="w-[22px] h-[22px]"
+          className="h-[22px] w-[22px]"
         />
         <div className="w-[9px]" />
-        <p className="bodymdmedium">5.0</p>
+        <p className="bodymdmedium">{review.rating.toFixed(2)}</p>
         <div className="w-[17px]" />
-        <div className="px-[5px] text-white bodyxsbold bg-gray-500 justify-center items-center">
+        <div className="bodyxsbold items-center justify-center bg-gray-500 px-[5px] text-white">
           Best
         </div>
       </div>
-      <p className="text-gray-700 bodymdregular">
-        임솔비 상담사님을 만나 문제를 해결 할 수 있었고 칭찬내용칭찬내용칭찬내용칭찬내용
-        칭찬내용칭찬내용칭찬내용칭찬내용
-      </p>
+      <p className="bodymdregular text-gray-700">{review.content}</p>
       <div className="flex flex-row justify-between text-gray-500">
-        <p className="bodysmregular">2024.05.06</p>
+        <p className="bodysmregular">{new Date(review.createdAt).toLocaleDateString()}</p>
         <p className="bodysmregular">엔데이 프리미엄 구매</p>
       </div>
     </div>
