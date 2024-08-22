6e7b3c606f172b69dc9f3281e98f81c43a650c11
gmltn328 <gmltn328@gmail.com>
2024-08-23 00:15:48 +0900

    Feat: 상담내역 게시글 기능구현 및 코드 리팩토링

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -1,12 +1,19 @@
 import Footer from '../components/Footer'
 import Header from '../components/header/Header'
-import Contents from '../components/post/Contents'
-
+import Contents from '../components/history/post/Contents.tsx'
+import { useParams } from 'react-router-dom'
+// 전체 상당 내역 개시글
 const Post: React.FC = () => {
+  const { id } = useParams()
+
+  if (!id) {
+    return <div>Error: Post ID is missing</div>
+  }
+
   return (
     <div className="flex flex-col items-center justify-center bg-white">
       <Header />
-      <Contents />
+      <Contents id={id} />
       <Footer />
     </div>
   )
