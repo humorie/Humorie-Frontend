b28c0c5b74c5fcfa7ef08e7aa83d56a7c55d9975
ddhhy <donghoney.k@gmail.com>
2024-08-23 16:43:49 +0900

    design: 개인정보수집및이용 페이지 컴포넌트 추가

---
+++ src/components/data_collection/DataHeader.tsx
@@ -0,0 +1,24 @@
+import { useNavigate } from 'react-router-dom'
+
+const DataHeader: React.FC = () => {
+  const navigate = useNavigate()
+
+  const handleBackClick = () => {
+    navigate(-1) // 뒤로가기
+  }
+
+  return (
+    <div className="flex h-[60px] w-full flex-row items-center bg-gray-800 px-[220px]">
+      <div className="flex flex-row items-center justify-between text-center">
+        <img
+          className="mr-[141px] h-[16px] w-[16px]"
+          src="/src/assets/images/icon/icon_arrow_left_white.svg"
+          alt="뒤로가기"
+          onClick={handleBackClick} // 뒤로가기 이벤트
+        />
+        <div className="bodylsemibold w-[770px] text-white">개인정보 수집 및 이용</div>
+      </div>
+    </div>
+  )
+}
+export default DataHeader
