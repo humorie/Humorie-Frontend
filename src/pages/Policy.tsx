f8b70fe986825b2d0556fc8d2e3455b66fcd7aa0
ddhhy <donghoney.k@gmail.com>
2024-08-23 16:45:50 +0900

    design: 이용약관 페이지 수정

--- src/pages/Policy.tsx
+++ src/pages/Policy.tsx
@@ -2,8 +2,6 @@ import { useEffect } from 'react'
 import Footer from '../components/Footer'
 import FirstArticle from '../components/policy/FirstArticle'
 import PolicyHeader from '../components/policy/PolicyHeader'
-import SecondArticle from '../components/policy/SecondArticle'
-import ThirdArticle from '../components/policy/ThirdArticle'
 
 const Policy: React.FC = () => {
   useEffect(() => {
@@ -15,13 +13,8 @@ const Policy: React.FC = () => {
   return (
     <div className="justify-top flex flex-col items-center text-black ">
       <PolicyHeader />
-      <div className="mb-[340px] mt-[48px] w-[1100px] space-y-[40px] px-[20px]">
-        <div className="bodymdmedium text-gray-400">
-          앤데이 서비스 이용에 필요한 사항을 규정합니다.
-        </div>
+      <div className="mb-[150px] mt-[48px] w-[1100px] px-[110px]">
         <FirstArticle />
-        <SecondArticle />
-        <ThirdArticle />
       </div>
       <Footer />
     </div>
