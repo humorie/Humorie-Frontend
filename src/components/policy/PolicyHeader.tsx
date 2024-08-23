8c62df51885e0be6b27bf9fdb7fd981b4877bb09
ddhhy <donghoney.k@gmail.com>
2024-08-23 16:44:42 +0900

    design: 이용약관 페이지 컴포넌트 추가 및 삭제

--- src/components/policy/PolicyHeader.tsx
+++ src/components/policy/PolicyHeader.tsx
@@ -8,10 +8,10 @@ const PolicyHeader: React.FC = () => {
   }
 
   return (
-    <div className="flex h-[60px] w-[1440px] flex-row items-center bg-gray-900 px-[170px]">
+    <div className="flex h-[60px] w-full flex-row items-center bg-gray-800 px-[220px]">
       <div className="flex flex-row items-center justify-between text-center">
         <img
-          className="mr-[133px] h-[16px] w-[16px]"
+          className="mr-[141px] h-[16px] w-[16px]"
           src="/src/assets/images/icon/icon_arrow_left_white.svg"
           alt="뒤로가기"
           onClick={handleBackClick} // 뒤로가기 이벤트
