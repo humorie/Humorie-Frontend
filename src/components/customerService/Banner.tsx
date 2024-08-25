50ba88e1295c6c2c9ade68693326a143cb75a62e
gmltn328 <gmltn328@gmail.com>
2024-08-25 19:28:49 +0900

    Refactor: 배너를 Banner 컴포넌트로 분리하여 적용

---
+++ src/components/customerService/Banner.tsx
@@ -0,0 +1,10 @@
+export const Banner = () => {
+  return (
+    <div className="relative mt-12 text-center text-black">
+      <img src="/src/assets/images/customerService/cs_rending_image1.svg" alt="렌딩이미지" />
+      <div className="xlbold absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 font-bold">
+        공지사항
+      </div>
+    </div>
+  )
+}
